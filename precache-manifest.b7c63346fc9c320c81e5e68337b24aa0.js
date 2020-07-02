795d5281ad256e8640543e728d2110404483a8fb
akheggde <anilhegde006@gmail.com>
2020-07-02 12:36:34 +0530

    Updates

---
+++ precache-manifest.b7c63346fc9c320c81e5e68337b24aa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4586cd534331f28b9934d571504a6213",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "073961a2e9e0e84be858",
+    "url": "/monsters-rolodex/static/css/main.29817ad5.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "073961a2e9e0e84be858",
+    "url": "/monsters-rolodex/static/js/main.070e4459.chunk.js"
+  },
+  {
+    "revision": "dfba82531f669c6858b0",
+    "url": "/monsters-rolodex/static/js/runtime-main.589ddcef.js"
+  }
+]);
